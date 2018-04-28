18544a5c634f6ff1c9204712e276f9d02c7251d4
Gugu Mabuza <mabuzagu@gmail.com>
2018-04-28 13:07:41 -0400

    setup travis

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,6 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
-import { doesNotReject } from 'assert';
 
 
 describe('Our first test', () => {
@@ -11,10 +10,8 @@ describe('Our first test', () => {
 })
 
 describe('index.hmtl', () => {
-
     it('should say hello', (done) => {
         const index =  fs.readFileSync('./src/index.html', "utf-8");
-
         jsdom.env(index, function(err, window){
             const h1 = window.document.getElementsByTagName('h1')[0];
             expect(h1.innerHTML).to.equal("Hello Gugu!");
