5fd179bd1e072c900298374d10b71c8317526a19
Gugu Mabuza <mabuzagu@gmail.com>
2018-04-28 14:58:10 -0400

    Simple http

--- src/index.test.js
+++ src/index.test.js
@@ -10,11 +10,11 @@ describe('Our first test', () => {
 })
 
 describe('index.hmtl', () => {
-    it('should say hello', (done) => {
+    it('should say users', (done) => {
         const index =  fs.readFileSync('./src/index.html', "utf-8");
         jsdom.env(index, function(err, window){
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello Gugu!");
+            expect(h1.innerHTML).to.equal("Users");
             done();
             window.close();    
         });
