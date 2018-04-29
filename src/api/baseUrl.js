23c32de2b0fa225b5a0ce82477fa573f138a07c3
Gugu Mabuza <mabuzagu@gmail.com>
2018-04-29 06:36:41 -0400

    Prod base url

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://evening-badlands-94401.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
